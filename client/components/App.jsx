807a96389b6539eaa8127e252caaed36148e877a
Kevin Gil <93kevingil@gmail.com>
2021-07-06 23:02:42 -0400

    added temp header photo and positioning of page links

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <div className="main">Welcome To My Website</div>
+    <div className="main">
+      <div className="image-container">
+        <img
+          src="/images/screenshot.png"
+          alt="Main Page Image Here"
+          className="header-image"
+        />
+        <div className="page-links">
+          <a className="link">Code</a>
+          <a className="link">Music</a>
+          <a className="link">Bio</a>
+        </div>
+      </div>
+      <div className="project-list"></div>
     </div>
   );
 };
