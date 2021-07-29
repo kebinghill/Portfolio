77138bc8fa43c99962c0baed44998ac3ceeba99c
Kevin Gil <93kevingil@gmail.com>
2021-07-28 23:13:11 -0400

    moved headshot over header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,14 +4,20 @@ import SocialLinks from './SocialLinks';
 
 const Header = () => {
   return (
-    <div className="image-container">
+    <div>
       <img
         src="/images/screenshot.png"
         alt="Art Image"
         className="header-image"
       />
-      <div className="title">KEVIN GIL</div>
-      <div className="sub-title">SOFTWARE ENGINEER</div>
+      <div className="title header-overlay">KEVIN GIL</div>
+      <div className="sub-title header-overlay">SOFTWARE ENGINEER</div>
+      <img
+        src="./images/headshot1.png"
+        alt="Headshot Photo"
+        id="headshot"
+        className="header-overlay"
+      />
       <PageLinks />
       <SocialLinks />
     </div>
