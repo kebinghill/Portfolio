40e27e1e095678769bb5c30232fc7851674c2785
Kevin Gil <93kevingil@gmail.com>
2021-09-11 16:05:55 -0400

    testing three.js react libraries

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ProjectList from './ProjectList';
 import BioSection from './BioSection';
 import Header from './Header';
-
+import Three from './Three';
 const App = () => {
   return (
     <div>
       <Header />
       <BioSection />
       <ProjectList />
+      {/* <Three /> */}
     </div>
   );
 };
---
+++ client/components/Three.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls, Stars } from '@react-three/drei';
+
+// function Box() {
+//   return (
+//     <mesh>
+//       <boxBufferGeometry attach="geometry" />
+//       <meshLambertMaterial attach="material" color="hotpink" />
+//     </mesh>
+//   );
+// }
+
+// const Three = () => {
+//   return (
+//     <Canvas>
+//       <OrbitControls />
+//       <Stars />
+//       <ambientLight intensity="0.5" />
+//       <spotLight position={[10, 15, 10]} angle={0.5} />
+//       <Box />
+//     </Canvas>
+//   );
+// };
+
+// export default Three;
