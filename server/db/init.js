029bca8cf2d597b3d7f38c1604fb56dd27596846
Kevin Gil <93kevingil@gmail.com>
2021-12-27 20:04:37 -0500

    changed database URL on new mac

--- server/db/init.js
+++ server/db/init.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/kgil'
+  process.env.DATABASE_URL || 'postgres://localhost/kebinghill'
 );
 
 module.exports = db;
