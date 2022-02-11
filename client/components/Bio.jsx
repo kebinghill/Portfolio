e33da6327022ce06ad4adab5194c0b1713e775a5
Kevin Gil <93kevingil@gmail.com>
2022-02-11 18:58:27 -0500

    added text for bio and todo for later

--- client/components/Bio.jsx
+++ client/components/Bio.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Bio = () => {
   return (
     <div className="section-container">
-      <h1>Under Construction</h1>
+      {/* TODO: need to configure css for bio and add resume image/doc below in separate container*/}
+      <div className="bio-section">
+        Hi, my name is Kevin and I am a software engineer/musician. I graduated
+        from Fullstack Academy's Flex Web Development program in July of 2021.
+        Before Fullstack Academy, I took various courses offered on Udemy and
+        Codecademy, both sites I still use to this day. I always enjoy the
+        process of learning something new and challenging, things that software
+        engineering has no shortage of. Check out my resume below for my work
+        history and/or navigate to the code tab above to check out some projects
+        I've created. Thanks for checking out my site and I hope to hear from
+        you soon!
+      </div>
     </div>
   );
 };
