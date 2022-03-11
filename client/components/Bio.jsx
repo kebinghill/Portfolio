d36efa5b75d132f659d3284a73957e8399728348
Kevin Gil <93kevingil@gmail.com>
2022-03-11 19:54:32 -0300

    made bio mobile friendly

--- client/components/Bio.jsx
+++ client/components/Bio.jsx
@@ -5,15 +5,17 @@ const Bio = () => {
     <div className="section-container">
       {/* TODO: need to configure css for bio and add resume image/doc below in separate container*/}
       <div className="bio-section">
-        Hi, my name is Kevin and I am a software engineer/musician. I graduated
-        from Fullstack Academy's Flex Web Development program in July of 2021.
-        Before Fullstack Academy, I took various courses offered on Udemy and
-        Codecademy, both sites I still use to this day. I always enjoy the
-        process of learning something new and challenging, things that software
-        engineering has no shortage of. Check out my resume below for my work
-        history and/or navigate to the code tab above to check out some projects
-        I've created. Thanks for checking out my site and I hope to hear from
-        you soon!
+        <p className="bio-text">
+          Hi, my name is Kevin and I am a software engineer/musician. I
+          graduated from Fullstack Academy's Flex Web Development program in
+          July of 2021. Before Fullstack Academy, I took various courses offered
+          on Udemy and Codecademy, both sites I still use to this day. I always
+          enjoy the process of learning something new and challenging, things
+          that software engineering has no shortage of. Check out my resume
+          below for my work history and/or navigate to the code tab above to
+          check out some projects I've created. Thanks for checking out my site
+          and I hope to hear from you soon!
+        </p>
       </div>
     </div>
   );
